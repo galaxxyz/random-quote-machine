ef2880d68f1cce4f28c59721061724ba9497556a
Kate <92258556+galaxxyz@users.noreply.github.com>
2021-10-18 14:00:04 +0300

    completed react project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+
+
+let quotesDBUrl = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+
+let colorsArray = ["c77dff", "ffd166", "f48498", "99d98c", "ffa033", "62bfed", "3590f3", "ff686b"];
+
+const useFetch = url => {
+  const [quotesArray, setQuotesArray] = useState(null);
+
+  async function fetchQuotes() {
+    const response = await fetch(url);
+    const parsedJSON = await response.json();
+    setQuotesArray(parsedJSON.quotes);
+  }
+
+  useEffect(() => { fetchQuotes() }, [url]);
+  return quotesArray;
+};
 
 function App() {
+  const quotesArray = useFetch(quotesDBUrl);
+  const [quote, setQuote] = useState({ quote: "", author: "" });
+  const [color, setColor] = useState('000000');
+
+  useEffect(() => {
+    if (quotesArray) {
+      changeQuote()
+    }
+  }, [quotesArray])
+
+  const getRandomItem = (array) => {
+    let randomInteger = Math.floor(array.length * Math.random());
+    return array[randomInteger];
+  }
+
+  const changeQuote = () => {
+    setQuote(getRandomItem(quotesArray));
+    setColor(getRandomItem(colorsArray));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header transition" style={{ backgroundColor: `#${color}` }}>
+        <div id="quote-box" className="transition" style={{ color: `#${color}` }}>
+          <p id="text"><FontAwesomeIcon icon={faQuoteLeft} /> {quote.quote}</p>
+          <p id="author">— {quote.author}</p>
+          <div id="buttons">
+            <a id="tweet-quote" className="transition" style={{ backgroundColor: `#${color}` }} href={encodeURI(`http://www.twitter.com/intent/tweet?text="${quote.quote}" -${quote.author}`)} target="_blank" rel="noreferrer" >
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <button id="new-quote" className="transition" style={{ backgroundColor: `#${color}` }} onClick={() => changeQuote()}>New Quote</button>
+          </div>
+        </div>
       </header>
     </div>
   );
